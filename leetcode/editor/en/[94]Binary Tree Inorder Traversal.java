29205224bb94102db2d2906f2301624f42fb6b5c
JJerrychan <164768186@qq.com>
2022-07-08 18:45:13 -0400

    binary tree

---
+++ leetcode/editor/en/[94]Binary Tree Inorder Traversal.java
@@ -0,0 +1,77 @@
+//Given the root of a binary tree, return the inorder traversal of its nodes' 
+//values. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: root = [1,null,2,3]
+//Output: [1,3,2]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: root = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: root = [1]
+//Output: [1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the tree is in the range [0, 100]. 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//Follow up: Recursive solution is trivial, could you do it iteratively? 
+//Related Topics Stack Tree Depth-First Search Binary Tree 👍 8218 👎 379
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<Integer>();
+        if (root == null) return list;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (!stack.empty() || root != null) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            list.add(root.val);
+            root = root.right;
+        }
+        return list;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
