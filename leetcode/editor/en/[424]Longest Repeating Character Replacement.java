b3068503beaa761ec9d1005f9e09d2590d0509aa
JJerrychan <164768186@qq.com>
2022-08-01 17:22:25 -0400

    [424]Longest Repeating Character Replacement

---
+++ leetcode/editor/en/[424]Longest Repeating Character Replacement.java
@@ -0,0 +1,61 @@
+//You are given a string s and an integer k. You can choose any character of 
+//the string and change it to any other uppercase English character. You can perform 
+//this operation at most k times. 
+//
+// Return the length of the longest substring containing the same letter you 
+//can get after performing the above operations. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "ABAB", k = 2
+//Output: 4
+//Explanation: Replace the two 'A's with two 'B's or vice versa.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "AABABBA", k = 1
+//Output: 4
+//Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+//The substring "BBBB" has the longest repeating letters, which is 4.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length <= 10⁵ 
+// s consists of only uppercase English letters. 
+// 0 <= k <= s.length 
+// 
+//
+// Related Topics Hash Table String Sliding Window 👍 5475 👎 215
+
+
+import java.util.HashMap;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int longest = 0, max = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int start = 0, end = 0;
+        while (end < s.length()) {
+            char ce = s.charAt(end);
+            map.put(ce, map.getOrDefault(ce, 0) + 1);
+            end++;
+            longest = Math.max(longest, map.get(ce));
+            if (end - start - longest > k) {
+                char cs = s.charAt(start);
+                map.put(cs, map.get(cs) - 1);
+                start++;
+            }
+            max = Math.max(max, end - start);
+        }
+        return max;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/en/[438]Find All Anagrams in a String.java
@@ -0,0 +1,72 @@
+//Given two strings s and p, return an array of all the start indices of p's 
+//anagrams in s. You may return the answer in any order. 
+//
+// An Anagram is a word or phrase formed by rearranging the letters of a 
+//different word or phrase, typically using all the original letters exactly once. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: s = "cbaebabacd", p = "abc"
+//Output: [0,6]
+//Explanation:
+//The substring with start index = 0 is "cba", which is an anagram of "abc".
+//The substring with start index = 6 is "bac", which is an anagram of "abc".
+// 
+//
+// Example 2: 
+//
+// 
+//Input: s = "abab", p = "ab"
+//Output: [0,1,2]
+//Explanation:
+//The substring with start index = 0 is "ab", which is an anagram of "ab".
+//The substring with start index = 1 is "ba", which is an anagram of "ab".
+//The substring with start index = 2 is "ab", which is an anagram of "ab".
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= s.length, p.length <= 3 * 10⁴ 
+// s and p consist of lowercase English letters. 
+// 
+//
+// Related Topics Hash Table String Sliding Window 👍 8197 👎 266
+
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new LinkedList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0, right = 0;
+        for (char c : p.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
+        int toBeMatched = map.size();
+        while (right < s.length()) {
+            char curr = s.charAt(right);
+            if (map.containsKey(curr)) {
+                map.put(curr, map.get(curr) - 1);
+                if (map.get(curr) == 0) toBeMatched--;
+            }
+            right++;
+            if (right - left > p.length()) {
+                char curl = s.charAt(left);
+                if (map.containsKey(curl)) {
+                    if (map.get(curl) == 0) toBeMatched++;
+                    map.put(curl, map.get(curl) + 1);
+                }
+                left++;
+            }
+            if (toBeMatched == 0) list.add(left);
+        }
+        return list;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
