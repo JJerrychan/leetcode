8814218c47ca680ad8f6630d13e248af1c2f6936
JJerrychan <164768186@qq.com>
2022-08-08 23:19:28 -0400

    [148]Sort List

---
+++ leetcode/editor/en/[148]Sort List.java
@@ -0,0 +1,86 @@
+//Given the head of a linked list, return the list after sorting it in 
+//ascending order. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: head = [4,2,1,3]
+//Output: [1,2,3,4]
+// 
+//
+// Example 2: 
+// 
+// 
+//Input: head = [-1,5,3,4,0]
+//Output: [-1,0,3,4,5]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: head = []
+//Output: []
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range [0, 5 * 10⁴]. 
+// -10⁵ <= Node.val <= 10⁵ 
+// 
+//
+// 
+// Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.
+//e. constant space)? 
+//
+// Related Topics Linked List Two Pointers Divide and Conquer Sorting Merge 
+//Sort 👍 7846 👎 246
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode getMid(ListNode head) {
+        ListNode slow = head, fast = head, prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        return slow;
+    }
+
+    public ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode right = getMid(head);
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(right);
+        return merge(l1, l2);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
