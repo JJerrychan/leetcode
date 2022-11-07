2feebef93b834412e0287fbb5c8cab7ca359fbe1
JJerrychan <164768186@qq.com>
2022-11-07 17:04:09 -0500

    [42]Trapping Rain Water

---
+++ leetcode/editor/en/[42]Trapping Rain Water.java
@@ -0,0 +1,56 @@
+//Given n non-negative integers representing an elevation map where the width 
+//of each bar is 1, compute how much water it can trap after raining. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+//Output: 6
+//Explanation: The above elevation map (black section) is represented by array [
+//0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) 
+//are being trapped.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: height = [4,2,0,3,2,5]
+//Output: 9
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// n == height.length 
+// 1 <= n <= 2 * 10⁴ 
+// 0 <= height[i] <= 10⁵ 
+// 
+//
+// Related Topics Array Two Pointers Dynamic Programming Stack Monotonic Stack ?
+//? 23724 👎 330
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int trap(int[] height) {
+        int l = 0, r = height.length - 1;
+        int lMax = 0, rMax = 0, sum = 0;
+        while (l < r) {
+            if (height[l] <= height[r]) {
+                if (lMax < height[l]) {
+                    lMax = height[l];
+                } else sum += lMax - height[l];
+                l++;
+            } else {
+                if (rMax < height[r]) {
+                    rMax = height[r];
+                } else sum += rMax - height[r];
+                r--;
+            }
+        }
+        return sum;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
