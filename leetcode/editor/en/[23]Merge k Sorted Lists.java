d05a473742fc6f2a0553c4e39847047f9da27eb2
JJerrychan <164768186@qq.com>
2022-11-07 15:47:11 -0500

    [23]Merge k Sorted Lists

---
+++ leetcode/editor/en/[23]Merge k Sorted Lists.java
@@ -0,0 +1,89 @@
+//You are given an array of k linked-lists lists, each linked-list is sorted in 
+//ascending order. 
+//
+// Merge all the linked-lists into one sorted linked-list and return it. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: lists = [[1,4,5],[1,3,4],[2,6]]
+//Output: [1,1,2,3,4,4,5,6]
+//Explanation: The linked-lists are:
+//[
+//  1->4->5,
+//  1->3->4,
+//  2->6
+//]
+//merging them into one sorted list:
+//1->1->2->3->4->4->5->6
+// 
+//
+// Example 2: 
+//
+// 
+//Input: lists = []
+//Output: []
+// 
+//
+// Example 3: 
+//
+// 
+//Input: lists = [[]]
+//Output: []
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// k == lists.length 
+// 0 <= k <= 10⁴ 
+// 0 <= lists[i].length <= 500 
+// -10⁴ <= lists[i][j] <= 10⁴ 
+// lists[i] is sorted in ascending order. 
+// The sum of lists[i].length will not exceed 10⁴. 
+// 
+//
+// Related Topics Linked List Divide and Conquer Heap (Priority Queue) Merge 
+//Sort 👍 14707 👎 552
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+        for (ListNode node : lists) {
+            while (node != null) {
+                queue.offer(new ListNode(node.val));
+                node = node.next;
+            }
+        }
+        ListNode root = queue.poll(), temp = root;
+        while (!queue.isEmpty()) {
+            temp.next = queue.poll();
+            temp = temp.next;
+        }
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
