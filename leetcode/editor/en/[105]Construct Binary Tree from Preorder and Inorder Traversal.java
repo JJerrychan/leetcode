ec8bb6a3ff5a5e7ca52073aea4ac4216f97f096e
JJerrychan <164768186@qq.com>
2023-01-10 18:14:37 -0500

    [105]Construct Binary Tree from Preorder and Inorder Traversal

---
+++ leetcode/editor/en/[105]Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -0,0 +1,74 @@
+//Given two integer arrays preorder and inorder where preorder is the preorder 
+//traversal of a binary tree and inorder is the inorder traversal of the same tree,
+// construct and return the binary tree. 
+//
+// 
+// Example 1: 
+// 
+// 
+//Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+//Output: [3,9,20,null,null,15,7]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: preorder = [-1], inorder = [-1]
+//Output: [-1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= preorder.length <= 3000 
+// inorder.length == preorder.length 
+// -3000 <= preorder[i], inorder[i] <= 3000 
+// preorder and inorder consist of unique values. 
+// Each value of inorder also appears in preorder. 
+// preorder is guaranteed to be the preorder traversal of the tree. 
+// inorder is guaranteed to be the inorder traversal of the tree. 
+// 
+//
+// Related Topics Array Hash Table Divide and Conquer Tree Binary Tree 👍 11868 
+//👎 344
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.HashMap;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    int preOrderIdx = 0;
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) map.put(inorder[i], i);
+        return helper(preorder, 0, inorder.length - 1);
+    }
+
+    TreeNode helper(int[] preoder, int start, int end) {
+        if (start > end) return null;
+        TreeNode root = new TreeNode(preoder[preOrderIdx]);
+        preOrderIdx++;
+        root.left = helper(preoder, start, map.get(root.val)-1);
+        root.right = helper(preoder, map.get(root.val)+1, end);
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
