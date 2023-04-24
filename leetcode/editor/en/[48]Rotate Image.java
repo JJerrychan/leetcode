f2b5e3517e9fb9b462fa868fd7404dcc7faf9fe5
JJerrychan <164768186@qq.com>
2023-04-24 17:38:45 -0400

    [48]Rotate Image

--- leetcode/editor/en/[48]Rotate Image.java
+++ leetcode/editor/en/[48]Rotate Image.java
@@ -35,6 +35,23 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - i][j];
+                matrix[n - 1 - i][j] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    /*public void rotate(int[][] matrix) {
+        int n = matrix.length;
         for (int i = 0; i < (n + 1) / 2; i++) {
             for (int j = 0; j < n / 2; j++) {
                 int temp = matrix[n - 1 - j][i];
@@ -44,6 +61,6 @@ class Solution {
                 matrix[i][j] = temp;
             }
         }
-    }
+    }*/
 }
 //leetcode submit region end(Prohibit modification and deletion)
