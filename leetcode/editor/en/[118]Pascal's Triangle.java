a6f1d45a071e3d1e3429a991a9c00e62eb278269
JJerrychan <164768186@qq.com>
2023-05-05 17:01:37 -0400

    [118]Pascal's Triangle

---
+++ leetcode/editor/en/[118]Pascal's Triangle.java
@@ -0,0 +1,47 @@
+//Given an integer numRows, return the first numRows of Pascal's triangle. 
+//
+// In Pascal's triangle, each number is the sum of the two numbers directly 
+//above it as shown: 
+// 
+// 
+// Example 1: 
+// Input: numRows = 5
+//Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// 
+// Example 2: 
+// Input: numRows = 1
+//Output: [[1]]
+// 
+// 
+// Constraints: 
+//
+// 
+// 1 <= numRows <= 30 
+// 
+//
+// Related Topics Array Dynamic Programming 👍 9876 👎 319
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(List.of(1));
+        List<Integer> prev = ans.get(0);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            list.add(1);
+            for (int j = 1; j < i; j++) {
+                list.add(prev.get(j - 1) + prev.get(j));
+            }
+            list.add(1);
+            ans.add(list);
+            prev = list;
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
