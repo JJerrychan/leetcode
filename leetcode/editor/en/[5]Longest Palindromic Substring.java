eefee2c909279e46ac0a13b0679bd9a9c6f69bc6
JJerrychan <164768186@qq.com>
2023-07-11 17:51:10 -0400

    [5]Longest Palindromic Substring

--- leetcode/editor/en/[5]Longest Palindromic Substring.java
+++ leetcode/editor/en/[5]Longest Palindromic Substring.java
@@ -55,25 +55,55 @@ class Solution {
         }
         return longest;
     }*/
-    public String longestPalindrome(String s) {
-        String longest = "";
-        for (int i = 0; i < s.length(); i++) {
-            int l = i, r = i + 1;
-            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-                l--;
-                r++;
-            }
-            if (r - l - 1 > longest.length()) longest = s.substring(l + 1, r);
 
-            l = i;
-            r = i;
-            while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-                l--;
-                r++;
-            }
-            if (r - l - 1 > longest.length()) longest = s.substring(l + 1, r);
+    /*Dynamic Programming*/
+    /*public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int l = 0, r = 0;
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
         }
-        return longest;
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                l = i;
+                r = i + 1;
+            }
+        }
+        for (int diff = 2; diff < n; diff++) {
+            for (int i = 0; i < n - diff; i++) {
+                int j = i + diff;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    l = i;
+                    r = j;
+                }
+            }
+        }
+        return s.substring(l, r + 1);
+    }*/
+
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int start = 0, end = 0;
+        for (int i = 0; i < n; i++) {
+            int len = Math.max(expand(i, i, s), expand(i, i + 1, s));
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    int expand(int l, int r, String s) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
