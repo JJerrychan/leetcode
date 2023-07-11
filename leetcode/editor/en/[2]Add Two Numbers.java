065779dc77478e1027889d23fa6bca675888842b
JJerrychan <164768186@qq.com>
2023-07-11 18:17:31 -0400

    [2]Add Two Numbers

--- leetcode/editor/en/[2]Add Two Numbers.java
+++ leetcode/editor/en/[2]Add Two Numbers.java
@@ -53,7 +53,8 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    /*iteration*/
+    /*public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode node = new ListNode(0);
         ListNode prev = new ListNode(0);
         prev = node;
@@ -72,6 +73,21 @@ class Solution {
             node = node.next;
         }
         return prev.next;
+    }*/
+    /*recrusion*/
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) l1 = new ListNode(0);
+        if (l2 == null) l2 = new ListNode(0);
+
+        int sum = l1.val + l2.val;
+        if (sum >= 10) {
+            sum -= 10;
+            if (l1.next == null) l1.next = new ListNode(1);
+            else l1.next.val = l1.next.val + 1;
+        }
+
+        return new ListNode(sum, addTwoNumbers(l1.next, l2.next));
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
