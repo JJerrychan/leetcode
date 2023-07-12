275b0a135e9c42ee48fa9f89edf08baef737586d
JJerrychan <164768186@qq.com>
2023-07-12 16:15:35 -0400

    [15]3Sum

--- leetcode/editor/en/[15]3Sum.java
+++ leetcode/editor/en/[15]3Sum.java
@@ -52,6 +52,28 @@ import java.util.List;
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new LinkedList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int target = 0 - nums[i];
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum > target) right--;
+                else if (sum < target) left++;
+                else {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return ans;
+    }
+    /*public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> ans = new LinkedList<>();
         for (int i = 0; i < nums.length - 2; i++) {
@@ -73,6 +95,6 @@ class Solution {
             }
         }
         return ans;
-    }
+    }*/
 }
 //leetcode submit region end(Prohibit modification and deletion)
