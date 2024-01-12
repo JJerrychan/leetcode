a047a6728a588111ba2b626e750e1a82a8091177
JJerrychan <164768186@qq.com>
2024-01-12 10:51:42 -0500

    Optimized O(1) extra space for [238]

--- leetcode/editor/en/[238]Product of Array Except Self.java
+++ leetcode/editor/en/[238]Product of Array Except Self.java
@@ -36,17 +36,17 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] prefix = new int[nums.length];
         int[] ans = new int[nums.length];
-        prefix[0] = 1;
-        for (int i = 1; i < nums.length; i++) {
-            prefix[i] = nums[i-1] * prefix[i - 1];
+        ans[0] = nums[0];
+        for (int i = 1; i < nums.length - 1; i++) {
+            ans[i] = ans[i - 1] * nums[i];
         }
         int R = 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            ans[i] = prefix[i] * R;
+        for (int i = nums.length - 1; i > 0; i--) {
+            ans[i] = ans[i - 1] * R;
             R *= nums[i];
         }
+        ans[0] = R;
         return ans;
     }
 }
