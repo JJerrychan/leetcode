8dcf1d844e98215268d37c6517cf29eec11d5d01
JJerrychan <164768186@qq.com>
2025-04-20 22:29:53 -0400

    [349]Intersection of Two Arrays

---
+++ leetcode/editor/en/[349]Intersection of Two Arrays.java
@@ -0,0 +1,58 @@
+//Given two integer arrays nums1 and nums2, return an array of their 
+//intersection. Each element in the result must be unique and you may return the result in 
+//any order. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//Output: [9,4]
+//Explanation: [4,9] is also accepted.
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= nums1.length, nums2.length <= 1000 
+// 0 <= nums1[i], nums2[i] <= 1000 
+// 
+//
+// Related Topics Array Hash Table Two Pointers Binary Search Sorting 👍 6403 👎
+// 2319
+
+
+import java.util.HashSet;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        for (int num : nums1) set1.add(num);
+
+        HashSet<Integer> resultSet = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+        for (int num : resultSet) {
+            result[i++] = num;
+        }
+        return result;
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
